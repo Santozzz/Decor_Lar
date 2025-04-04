6c3f1963a48a9c5444f5d7498a793fd4aacfd8c8
Santozzz <davd.c9ol@gmail.com>
2025-04-04 16:11:15 -0300

    routes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,22 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {createBrowserRouter, RouterProvider, Route} from "react-router-dom"
+import Contact from './routes/Contact.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "contact",
+    element: <Contact />,
+  },
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
---
+++ frontend/src/routes/Contact.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div>Pagina de contato</div>
+  )
+}
+
+export default Contact
