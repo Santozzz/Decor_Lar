95f227748e1a8661ed28a41cc0c3298f203876ff
Santozzz <davd.c9ol@gmail.com>
2025-04-04 21:15:36 -0300

    UPDATE: Menu do sistema finalizado(responsivo)

--- frontend/src/assets/components/SISTEMA/Navbar_SIS.jsx
+++ frontend/src/assets/components/SISTEMA/Navbar_SIS.jsx
@@ -1,56 +1,54 @@
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
 import './../../styles/components/SISTEMA/Navbar_SIS.css'
-import React, { useState } from 'react';
-import { NavLink } from 'react-router-dom';
+
 
 const Navbar = () => {
-    
-    const [activeLink, setActiveLink] = useState('produtos');
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => setMenuOpen(!menuOpen);
 
     return (
-        <div>
-            <nav className="navbar-row">
+        <>
+            {/* Navbar superior */}
+            <div className="navbar-row">
                 <div className="space">
                     <div className="logo">
+                        {/* Botão hamburguer visível a partir de 1240px */}
+                        <button className="menu-toggle" onClick={toggleMenu}>
+                            <i className={`fa-solid ${menuOpen ? "fa-xmark" : "fa-bars"}`}></i>
+                        </button>
                         <p>Decor&Lar</p>
                     </div>
                     <i class="fa-solid fa-bell"></i>
                 </div>
-            </nav>
-            <nav className="navbar-column">
-                <ul className='navlist'>
-                    <li className={`link ${activeLink === 'produtos' ? 'active' : ''}`}onClick={() => setActiveLink('produtos')}>
-                        <NavLink className="NavLink" to="/Produtos">
-                        <div className="icon">
-                            <i className="fa-solid fa-boxes-stacked"></i>
-                        </div>
-                        <p>Produtos</p>
+            </div>
+
+            {/* Sidebar lateral */}
+            <nav className={`navbar-column ${menuOpen ? "open" : ""}`}>
+                <ul className="navlist">
+                    <li className="link">
+                        <NavLink className="NavLink" to="/Produtos" onClick={() => setMenuOpen(false)}>
+                            <div className="icon"><i className="fa-solid fa-boxes-stacked"></i></div>
+                            <p>Produtos</p>
                         </NavLink>
                     </li>
-
-                    <li className={`link ${activeLink === 'adicionar' ? 'active' : ''}`}onClick={() => setActiveLink('adicionar')}>
-                         <NavLink className="NavLink" to="/AdicionarProdutos">
-                            <div className="icon">
-                                <i class="fa-solid fa-cart-plus"></i>
-                            </div>
+                    <li className="link">
+                        <NavLink className="NavLink" to="/AdicionarProdutos" onClick={() => setMenuOpen(false)}>
+                            <div className="icon"><i className="fa-solid fa-cart-plus"></i></div>
                             <p>Adicionar Produtos</p>
                         </NavLink>
                     </li>
-
-                    <li
-                        className={`link ${activeLink === 'pedidos' ? 'active' : ''}`}
-                        onClick={() => setActiveLink('pedidos')}
-                    >
-                        <NavLink className="NavLink" to="/Pedidos">
-                            <div className="icon">
-                                <i class="fa-solid fa-list-ol"></i>     
-                            </div>
+                    <li className="link">
+                        <NavLink className="NavLink" to="/Pedidos" onClick={() => setMenuOpen(false)}>
+                            <div className="icon"><i className="fa-solid fa-list-ol"></i></div>
                             <p>Pedidos</p>
                         </NavLink>
                     </li>
                 </ul>
             </nav>
-        </div>
-    )
-}
+        </>
+    );
+};
 
-export default Navbar
+export default Navbar;
